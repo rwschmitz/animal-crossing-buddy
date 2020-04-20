02bda04fc2e9510c9fb37106a965e3db0349c566
rwschmitz <Rudolph.W.Schmitz@gmail.com>
2020-04-19 22:24:47 -0500

    Succesfully input data to frontend form, submit through backend server to update db!

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,37 +1,54 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 import mongodb from 'mongodb';
 
 dotenv.config();
 const app = express();
 const port = 3001;
 
-app.get('/', (_, res) => res.send('blinded by the lights'));
-app.get('/about', (_, res) => res.send('this is the about page'));
-
-app.listen(port, () => console.log(`Backend is listening on port ${port}`));
-
-const readRandomAirbnb = (airbnb: any) => {
-  console.log('airbnb', airbnb);
-};
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 const { MongoClient } = mongodb;
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@${process.env.DB_PATH}`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect((err): void => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log('Successfully connected to the server!');
 
-  // perform actions on the collection object
-  const collection = client.db('sample_airbnb').collection('listingsAndReviews');
-  collection.find({ _id: '10009999' }).toArray((err, docs) => {
+app.get('/', (_, res) => res.send('blinded by the lights'));
+app.get('/about', (_, res) => res.send('this is the about page'));
+app.get('/cats', (_, res) => {
+  client.connect((err): void => {
     if (err) {
       console.log(err);
+      return;
     }
-    readRandomAirbnb(docs);
+
+    const collection = client.db('animals').collection('cats');
+    collection
+      .find({})
+      .toArray()
+      .then((items) => {
+        res.status(200).json(items);
+      });
   });
-  // client.close();
 });
+
+app.post('/cats', (req, res) => {
+  res.send(req.statusCode);
+
+  client.connect((err): void => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const collection = client.db('animals').collection('cats');
+    collection.insertOne({
+      id: req.body.catId,
+      name: req.body.catName,
+      age: req.body.catAge,
+    });
+  });
+});
+
+app.listen(port, () => console.log(`Backend is listening on port ${port}`));
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,6 @@
-import React, { ReactElement } from 'react';
+import React, { FormEvent, ReactElement, useEffect, useState } from 'react';
+import axios from 'axios';
+import qs from 'qs';
 import styled from 'styled-components';
 
 const _Page = styled.section`
@@ -16,10 +18,82 @@ const _H1 = styled.h1`
   margin: 0;
 `;
 
+const _Container = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 0.5rem;
+  max-width: 275px;
+  width: 100%;
+  &:last-child {
+    margin-top: 1rem;
+  }
+`;
+
+const _Label = styled.label``;
+
+const _Form = styled.form`
+  border: 2px dashed white;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 2rem;
+  margin-bottom: 2rem;
+  margin-top: 2rem;
+  max-width: 500px;
+`;
+
 const Home = (): ReactElement => {
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      const res = await axios.get('http://localhost:3001/cats');
+      console.log(res);
+    };
+    fetchData();
+  }, []);
+
+  const [catId, setCatId] = useState('');
+  const [catName, setCatName] = useState('');
+  const [catAge, setCatAge] = useState('');
+
+  const addCat = async (event: FormEvent): Promise<void> => {
+    event.preventDefault();
+    const res = await axios({
+      method: 'post',
+      url: 'http://localhost:3001/cats',
+      data: qs.stringify({
+        catId,
+        catName,
+        catAge,
+      }),
+    });
+    console.log('post req -->', res);
+  };
   return (
     <_Page>
       <_H1>Animal Crossing Buddy</_H1>
+      <_Form onSubmit={(event): Promise<void> => addCat(event)}>
+        <_Container>
+          <_Label id='catId' htmlFor='catId'>
+            id
+          </_Label>
+          <input name='catId' onChange={(event): void => setCatId(event.target.value)} value={catId} />
+        </_Container>
+        <_Container>
+          <_Label id='catName' htmlFor='catName'>
+            name
+          </_Label>
+          <input name='catName' onChange={(event): void => setCatName(event.target.value)} value={catName} />
+        </_Container>
+        <_Container>
+          <_Label id='catAge' htmlFor='catAge'>
+            age
+          </_Label>
+          <input name='catAge' onChange={(event): void => setCatAge(event.target.value)} value={catAge} />
+        </_Container>
+        <_Container>
+          <input type='submit' value='submit' />
+        </_Container>
+      </_Form>
     </_Page>
   );
 };
