ab3b3b4b1ac17986ae9f3b4eddef4c860bb7d4a8
rwschmitz <Rudolph.W.Schmitz@gmail.com>
2020-04-19 20:05:28 -0500

    Connect to mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,21 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongodb from 'mongodb';
 
+dotenv.config();
 const app = express();
 const port = 3001;
 
 app.get('/', (_, res) => res.send('blinded by the lights'));
 
 app.listen(port, () => console.log(`Backend is listening on port ${port}`));
+
+const { MongoClient } = mongodb;
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@${process.env.DB_PATH}`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect((): void => {
+  const collection = client.db('test').collection('devices');
+  // perform actions on the collection object
+  console.log(collection);
+  client.close();
+});
