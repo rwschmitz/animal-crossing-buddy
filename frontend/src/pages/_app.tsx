9bc582f53a729f92333df0afa28eb5c2472b56ba
rwschmitz <Rudolph.W.Schmitz@gmail.com>
2020-04-22 01:15:36 -0500

    Try again

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from 'next/app';
 import '../../node_modules/normalize.css/normalize.css';
 import '../../index.css';
 
+// Using this pattern for global css
 const AnimalCrossingBuddy = ({ Component, pageProps }: AppProps): ReactElement => {
   return <Component {...pageProps} />;
 };
