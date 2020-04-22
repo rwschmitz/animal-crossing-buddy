d832aa06c7f345849362e06597c1bb7092a71c04
rwschmitz <Rudolph.W.Schmitz@gmail.com>
2020-04-21 22:08:46 -0500

    Add new keys

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,3 @@
-console.log({
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  databaseURL: process.env.DATABASEURL,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID,
-});
-
 module.exports = {
   env: {
     apiKey: process.env.APIKEY,
