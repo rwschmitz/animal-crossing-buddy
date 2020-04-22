5f1e57c35abfeffa46c6948d8fb9a7e80661c8b2
rwschmitz <Rudolph.W.Schmitz@gmail.com>
2020-04-21 21:22:47 -0500

    Add now.json file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,13 @@
+console.log({
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+});
+
 module.exports = {
   env: {
     apiKey: process.env.APIKEY,
