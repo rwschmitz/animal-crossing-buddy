201dbd43a67b6406f44d9c25a877c59d735df224
rwschmitz <Rudolph.W.Schmitz@gmail.com>
2020-04-21 23:58:52 -0500

    Trying with mongo again

--- frontend/src/pages/api/cats.ts
+++ frontend/src/pages/api/cats.ts
@@ -1,27 +1,29 @@
-// import mongodb from 'mongodb';
+import mongodb from 'mongodb';
 
 export default (_: any, res: any): void => {
-  res.statusCode = 200;
+  // res.statusCode = 200;
+  // res.setHeader('Content-Type', 'application/json');
+  // res.end(JSON.stringify({ name: 'blah' }));
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ name: 'blah' }));
-  // const { MongoClient } = mongodb;
-  // const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@${process.env.DB_PATH}`;
-  // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  // client.connect((err): void => {
-  //   if (err) {
-  //     console.log(err);
-  //     return;
-  //   }
+  const { MongoClient } = mongodb;
+  const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@${process.env.DB_PATH}`;
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect((err): void => {
+    if (err) {
+      console.log(err);
+      return;
+    }
 
-  //   const collection = client.db('animals').collection('cats');
-  //   collection
-  //     .find({})
-  //     .toArray()
-  //     .then((items) => {
-  //       res.status(200).json(items);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // });
+    const collection = client.db('animals').collection('cats');
+    collection
+      .find({})
+      .toArray()
+      .then((items) => {
+        res.status(200).json(items);
+        res.end(JSON.stringify(items));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 };
