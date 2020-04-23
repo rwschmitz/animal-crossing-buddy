3461bbea93a7ae226d0d38bb128028614ba07423
rwschmitz <Rudolph.W.Schmitz@gmail.com>
2020-04-22 21:36:10 -0500

    Flip back to mongodb call

--- frontend/src/pages/api/cats.ts
+++ frontend/src/pages/api/cats.ts
@@ -1,42 +1,42 @@
-export default async (_: any, res: any): Promise<void> => {
-  res.status(200).json({
-    name: 'rudy',
-    age: 31,
-    cool: true,
-  });
-  res.end();
-};
-// import mongodb from 'mongodb';
-// import { NowResponse } from '@now/node';
-
-// const { MongoClient } = mongodb;
-// const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@${process.env.DB_PATH}`;
-
-// const createMongoClient = (): mongodb.MongoClient => {
-//   let client;
-//   if (!client) {
-//     client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-//   }
-//   return client;
-// };
-
-// const connectToMongoClient = async (): Promise<mongodb.MongoClient> => {
-//   const client = createMongoClient();
-//   if (!client.isConnected()) {
-//     await client.connect();
-//   }
-//   return client;
-// };
-
-// const getCollection = async (): Promise<any> => {
-//   const client = await connectToMongoClient();
-//   const collection = client.db('animals').collection('cats');
-//   const data = await collection.find({}).toArray();
-//   return data;
-// };
-
-// export default async (_: any, res: NowResponse): Promise<void> => {
-//   const response = await getCollection();
-//   res.status(200).json(response);
+// export default async (_: any, res: any): Promise<void> => {
+//   res.status(200).json({
+//     name: 'rudy',
+//     age: 31,
+//     cool: true,
+//   });
 //   res.end();
 // };
+import mongodb from 'mongodb';
+import { NowResponse } from '@now/node';
+
+const { MongoClient } = mongodb;
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@${process.env.DB_PATH}`;
+
+const createMongoClient = (): mongodb.MongoClient => {
+  let client;
+  if (!client) {
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  }
+  return client;
+};
+
+const connectToMongoClient = async (): Promise<mongodb.MongoClient> => {
+  const client = createMongoClient();
+  if (!client.isConnected()) {
+    await client.connect();
+  }
+  return client;
+};
+
+const getCollection = async (): Promise<any> => {
+  const client = await connectToMongoClient();
+  const collection = client.db('animals').collection('cats');
+  const data = await collection.find({}).toArray();
+  return data;
+};
+
+export default async (_: any, res: NowResponse): Promise<void> => {
+  const response = await getCollection();
+  res.status(200).json(response);
+  res.end();
+};
