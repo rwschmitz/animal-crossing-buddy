54dfdac14f666d5b6c4ca5c8b697bb85b3c37d5f
rwschmitz <Rudolph.W.Schmitz@gmail.com>
2020-04-22 23:14:08 -0500

    Get rid of swr

--- frontend/src/pages/api/cats.ts
+++ frontend/src/pages/api/cats.ts
@@ -1,50 +1,34 @@
-// export default async (_: any, res: any): Promise<void> => {
-//   res.status(200).json({
-//     name: 'rudy',
-//     age: 31,
-//     cool: true,
-//   });
-//   res.end();
-// };
-// import mongodb from 'mongodb';
-// import { NowResponse } from '@now/node';
+import mongodb from 'mongodb';
+import { NowResponse } from '@now/node';
 
-// const { MongoClient } = mongodb;
-// const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@${process.env.DB_PATH}`;
-// console.log('USER_NAME', process.env.USER_NAME);
-// console.log('USER_PASS', process.env.USER_PASS);
-// console.log('DB_PATH', process.env.DB_PATH);
+const { MongoClient } = mongodb;
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@${process.env.DB_PATH}`;
 
-// const createMongoClient = (): mongodb.MongoClient => {
-//   let client;
-//   if (!client) {
-//     client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-//   }
-//   return client;
-// };
+const createMongoClient = (): mongodb.MongoClient => {
+  let client;
+  if (!client) {
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  }
+  return client;
+};
 
-// const connectToMongoClient = async (): Promise<mongodb.MongoClient> => {
-//   const client = createMongoClient();
-//   if (!client.isConnected()) {
-//     await client.connect();
-//   }
-//   return client;
-// };
+const connectToMongoClient = async (): Promise<mongodb.MongoClient> => {
+  const client = createMongoClient();
+  if (!client.isConnected()) {
+    await client.connect();
+  }
+  return client;
+};
 
-// const getCollection = async (): Promise<any> => {
-//   const client = await connectToMongoClient();
-//   const collection = client.db('animals').collection('cats');
-//   const data = await collection.find({}).toArray();
-//   return data;
-// };
+const getCollection = async (): Promise<any> => {
+  const client = await connectToMongoClient();
+  const collection = client.db('animals').collection('cats');
+  const data = await collection.find({}).toArray();
+  return data;
+};
 
-export default async (_: any, res: any): Promise<void> => {
-  res.status(200).json({
-    un: process.env.USER_NAME,
-    pw: process.env.USER_PASS,
-    dbpath: process.env.DB_PATH,
-  });
-  // const response = await getCollection();
-  // res.status(200).json(response);
-  // res.end();
+export default async (_: any, res: NowResponse): Promise<void> => {
+  const response = await getCollection();
+  res.status(200).json(response);
+  res.end();
 };
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,7 +4,7 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import qs from 'qs';
 import styled from 'styled-components';
-import useSwr from 'swr';
+// import useSwr from 'swr';
 
 const _Page = styled.section`
   background-color: black;
@@ -88,13 +88,22 @@ const Home = (): ReactElement => {
   }
   firebase.auth();
 
-  const fetcher = (url: any): any =>
-    axios
-      .get(url)
-      .then((res) => res.data)
-      .catch((error) => console.log(error));
+  // const fetcher = (url: any): any =>
+  //   axios
+  //     .get(url)
+  //     .then((res) => res.data)
+  //     .catch((error) => console.log(error));
 
-  const { data, error } = useSwr('/api/cats', fetcher);
+  // const { data, error } = useSwr('/api/cats', fetcher);
+
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      const res = await axios.get('/api/cats');
+      const { data } = res;
+      console.log(data);
+    };
+    fetchData();
+  }, []);
 
   const theCurrentUser = useAuth();
   // console.log('the current user', theCurrentUser);
@@ -177,91 +186,81 @@ const Home = (): ReactElement => {
   };
   return (
     <_Page>
-      {error && <div>error</div>}
-      {!data && <div>loading...</div>}
-      {data && (
-        <>
-          <_H1>Animal Crossing Buddy</_H1>
-          <section>
-            {theCurrentUser ? <_SignedInText>signed in!</_SignedInText> : <_SignedOutText>signed out!</_SignedOutText>}
-          </section>
-          <section>
-            <_H1>Sign up</_H1>
-            <_Form onSubmit={(event): void => addUser(event)}>
-              <_Container>
-                <_Label id='email' htmlFor='email'>
-                  email
-                </_Label>
-                <input name='email' onChange={(event): void => setEmail(event.target.value)} value={email} />
-              </_Container>
-              <_Container>
-                <_Label id='password' htmlFor='password'>
-                  password
-                </_Label>
-                <input name='password' onChange={(event): void => setPassword(event.target.value)} value={password} />
-              </_Container>
-              <_Container>
-                <input type='submit' value='submit' />
-              </_Container>
-            </_Form>
-          </section>
-          <section>
-            <_H1>Email sign in</_H1>
-            <_Form onSubmit={(event): void => signInUser(event)}>
-              <_Container>
-                <_Label id='userEmail' htmlFor='userEmail'>
-                  email
-                </_Label>
-                <input
-                  name='userEmail'
-                  onChange={(event): void => setUserEmail(event.target.value)}
-                  value={userEmail}
-                />
-              </_Container>
-              <_Container>
-                <_Label id='userPassword' htmlFor='userPassword'>
-                  password
-                </_Label>
-                <input
-                  name='userPassword'
-                  onChange={(event): void => setUserPassword(event.target.value)}
-                  value={userPassword}
-                />
-              </_Container>
-              <_Container>
-                <input type='submit' value='submit' />
-              </_Container>
-            </_Form>
-          </section>
-          <section>
-            <_H1>Add cat</_H1>
-            <_Form onSubmit={(event): Promise<void> => addCat(event)}>
-              <_Container>
-                <_Label id='catId' htmlFor='catId'>
-                  id
-                </_Label>
-                <input name='catId' onChange={(event): void => setCatId(event.target.value)} value={catId} />
-              </_Container>
-              <_Container>
-                <_Label id='catName' htmlFor='catName'>
-                  name
-                </_Label>
-                <input name='catName' onChange={(event): void => setCatName(event.target.value)} value={catName} />
-              </_Container>
-              <_Container>
-                <_Label id='catAge' htmlFor='catAge'>
-                  age
-                </_Label>
-                <input name='catAge' onChange={(event): void => setCatAge(event.target.value)} value={catAge} />
-              </_Container>
-              <_Container>
-                <input type='submit' value='submit' />
-              </_Container>
-            </_Form>
-          </section>
-          <button onClick={signOutUser}>sign out</button>
-        </>
-      )}
+      <_H1>Animal Crossing Buddy</_H1>
+      <section>
+        {theCurrentUser ? <_SignedInText>signed in!</_SignedInText> : <_SignedOutText>signed out!</_SignedOutText>}
+      </section>
+      <section>
+        <_H1>Sign up</_H1>
+        <_Form onSubmit={(event): void => addUser(event)}>
+          <_Container>
+            <_Label id='email' htmlFor='email'>
+              email
+            </_Label>
+            <input name='email' onChange={(event): void => setEmail(event.target.value)} value={email} />
+          </_Container>
+          <_Container>
+            <_Label id='password' htmlFor='password'>
+              password
+            </_Label>
+            <input name='password' onChange={(event): void => setPassword(event.target.value)} value={password} />
+          </_Container>
+          <_Container>
+            <input type='submit' value='submit' />
+          </_Container>
+        </_Form>
+      </section>
+      <section>
+        <_H1>Email sign in</_H1>
+        <_Form onSubmit={(event): void => signInUser(event)}>
+          <_Container>
+            <_Label id='userEmail' htmlFor='userEmail'>
+              email
+            </_Label>
+            <input name='userEmail' onChange={(event): void => setUserEmail(event.target.value)} value={userEmail} />
+          </_Container>
+          <_Container>
+            <_Label id='userPassword' htmlFor='userPassword'>
+              password
+            </_Label>
+            <input
+              name='userPassword'
+              onChange={(event): void => setUserPassword(event.target.value)}
+              value={userPassword}
+            />
+          </_Container>
+          <_Container>
+            <input type='submit' value='submit' />
+          </_Container>
+        </_Form>
+      </section>
+      <section>
+        <_H1>Add cat</_H1>
+        <_Form onSubmit={(event): Promise<void> => addCat(event)}>
+          <_Container>
+            <_Label id='catId' htmlFor='catId'>
+              id
+            </_Label>
+            <input name='catId' onChange={(event): void => setCatId(event.target.value)} value={catId} />
+          </_Container>
+          <_Container>
+            <_Label id='catName' htmlFor='catName'>
+              name
+            </_Label>
+            <input name='catName' onChange={(event): void => setCatName(event.target.value)} value={catName} />
+          </_Container>
+          <_Container>
+            <_Label id='catAge' htmlFor='catAge'>
+              age
+            </_Label>
+            <input name='catAge' onChange={(event): void => setCatAge(event.target.value)} value={catAge} />
+          </_Container>
+          <_Container>
+            <input type='submit' value='submit' />
+          </_Container>
+        </_Form>
+      </section>
+      <button onClick={signOutUser}>sign out</button>
     </_Page>
   );
 };
