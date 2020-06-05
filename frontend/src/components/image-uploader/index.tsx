c0bda7b19bc4c8ec6156667d5f555bae8a20b551
rwschmitz <Rudolph.W.Schmitz@gmail.com>
2020-06-05 02:28:09 -0500

    Add cloudfront

--- frontend/src/components/image-uploader/index.tsx
+++ frontend/src/components/image-uploader/index.tsx
@@ -1,13 +1,16 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import { Auth, Storage } from 'aws-amplify';
 
 const ImageUploader = (): ReactElement => {
+  const [imageKey, setImageKey] = useState('');
   const handleChange = (event: any): void => {
     event.persist(); // needed for accessing event.target ... remove it to see error ... something about synthetic event pooling
     Auth.currentCredentials()
       .then(() => {
         const file = event.target.files[0];
-        Storage.put('rws-example-001', file) // create key with UUID?, then store this key in mongodb, and then fetch urls from mongodb to get images from aws
+        console.log(event.target.files);
+        const name = file.name;
+        Storage.put(name, file) // create key with UUID?, then store this key in mongodb, and then fetch urls from mongodb to get images from aws
           .then((res) => console.log(res))
           .catch((error) => console.log(error));
       })
@@ -38,12 +41,23 @@ const ImageUploader = (): ReactElement => {
       });
   };
 
+  const getImage = (): void => {
+    Auth.currentCredentials().then(() => {
+      Storage.get('gatsby-astronaut.png').then((res: any) => {
+        const str = res.split('/public/')[1].split('?')[0];
+        setImageKey(str);
+      });
+    });
+  };
+
   return (
     <div>
       <h2>IMAGE UPLOADER</h2>
       <input type='file' accept='image/*' onChange={(event): void => handleChange(event)} />
       <button onClick={(): void => handleClick()}>upload blank text file</button>
       <button onClick={(): void => getMoreInfo()}>get info</button>
+      <img src={`https://dq4qf9v6l2li8.cloudfront.net/public/${imageKey}`} alt='cloudfront test' />
+      <button onClick={(): void => getImage()}>get image</button>
     </div>
   );
 };
