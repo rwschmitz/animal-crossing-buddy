6825911dc595762c0f876afeb24679c2913cc307
rwschmitz <Rudolph.W.Schmitz@gmail.com>
2020-06-07 18:45:10 -0500

    Remove firebase

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   env: {
-    apiKey: process.env.APIKEY,
-    authDomain: process.env.AUTHDOMAIN,
-    databaseURL: process.env.DATABASEURL,
-    projectId: process.env.PROJECTID,
-    storageBucket: process.env.STORAGEBUCKET,
-    messagingSenderId: process.env.MESSAGINGSENDERID,
-    appId: process.env.APPID,
     USER_NAME: process.env.USER_NAME,
     USER_PASS: process.env.USER_PASS,
     DB_PATH: process.env.DB_PATH,
--- frontend/src/utils/init-amplify/index.ts
+++ frontend/src/utils/init-amplify/index.ts
@@ -1,5 +1,5 @@
 import Amplify from 'aws-amplify';
-import awsconfig from '../../aws-config';
+import awsconfig from '../../aws-exports';
 
 const initAmplify = (): void => {
   Amplify.configure(awsconfig);
